824600fbc87caed25aa0c66581be9484e56b434b
Severino Mateus Jr <severmateus@gmail.com>
2015-06-18 15:54:33 +0200

    Rejected commit: Default

--- app/controllers/NossaHistoriaController.php
+++ app/controllers/NossaHistoriaController.php
@@ -13,6 +13,6 @@ class NossaHistoriaController extends BaseController {
     }
 
     public function doPergunta (){
-        return "GITHUB FUNCIONA MESMO SEM INSTALAR A APLICAÇÃO DO NGOMANA";
+        return "GITHUB FUNCIONA MESMO SEM INSTALAR A APLICAÇÃO DO NGOMANA E SEVERINO";
     }
 }
