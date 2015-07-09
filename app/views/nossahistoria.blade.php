c37c12fb35965c76c77497aa09b5605650ed208d
João Rodrigues <rodrigueslcn@hotmail.com>
2015-07-09 12:20:43 +0200

    Actualização da vista nossahistoria

--- app/views/nossahistoria.blade.php
+++ app/views/nossahistoria.blade.php
@@ -7,6 +7,44 @@
 
     <?php
     $pergunta = Pergunta::all()->first();
+    $numero = rand(1,5);
+     if($numero==1){
+         $resposta1 = $pergunta->resposta1;
+         $resposta2 = $pergunta->resposta2;
+         $resposta3 = $pergunta->resposta3;
+         $resposta4 = $pergunta->resposta4;
+         $resposta5 = $pergunta->resposta_certa;
+
+     }
+            elseif($numero==2){
+                $resposta2 = $pergunta->resposta1;
+                $resposta3 = $pergunta->resposta2;
+                $resposta4 = $pergunta->resposta3;
+                $resposta5 = $pergunta->resposta4;
+                $resposta1 = $pergunta->resposta_certa;
+            }
+            elseif($numero==3){
+                $resposta3 = $pergunta->resposta1;
+                $resposta4 = $pergunta->resposta2;
+                $resposta5 = $pergunta->resposta3;
+                $resposta1 = $pergunta->resposta4;
+                $resposta2 = $pergunta->resposta_certa;
+            }
+            elseif($numero==4){
+                $resposta4 = $pergunta->resposta1;
+                $resposta5 = $pergunta->resposta2;
+                $resposta1 = $pergunta->resposta3;
+                $resposta2 = $pergunta->resposta4;
+                $resposta3 = $pergunta->resposta_certa;
+            }
+
+            elseif($numero==5){
+                $resposta5 = $pergunta->resposta1;
+                $resposta1 = $pergunta->resposta2;
+                $resposta2 = $pergunta->resposta3;
+                $resposta3 = $pergunta->resposta4;
+                $resposta4 = $pergunta->resposta_certa;
+            }
     ?>
 
     <div class="container">
@@ -14,33 +52,45 @@
 
             <script>
                 function alteraResposta(respostaEscolhida,idEscolhido){
+                    deSeleciona();
                     document.getElementById('respostaEscolhida').setAttribute('value',respostaEscolhida);
+                    document.getElementById(idEscolhido).setAttribute('class','resposta');
+                }
+
+                function deSeleciona(){
+                    for (i=1;i<=5;i++)
+                        document.getElementById('resp'+i).setAttribute('class','');
                 }
             </script>
 
             <h2>{{$pergunta->pergunta}}</h2>
             {{Form::open(array('url'=>'/nossahistoria'))}}
 
-            <p>
+            <p id="resp1">
                 {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta1',
-                'onclick'=>"alteraResposta($pergunta->resposta1,resposta1)",'class'=>'resposta'))}}
-                {{form::label('resposta1',$pergunta->resposta1,array('class'=>'resposta'))}}
+                'onclick'=>"alteraResposta($resposta1,'resp1')"))}}
+                {{form::label('resposta1',$resposta1,array('onclick'))}}
+            </p>
 
-              <p>  {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta2',
-              'onclick'=>"alteraResposta($pergunta->resposta2,resposta2)"))}}
-                {{form::label('resposta2',$pergunta->resposta2)}}
+              <p id="resp2">  {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta2',
+              'onclick'=>"alteraResposta($resposta2,'resp2')"))}}
+                {{form::label('resposta2',$resposta2)}}
+              </p>
 
-              <p> {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta3',
-              'onclick'=>"alteraResposta($pergunta->resposta_certa,resposta3)"))}}
-                {{form::label('resposta3',$pergunta->resposta_certa)}}
+              <p id="resp3"> {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta3',
+              'onclick'=>"alteraResposta($resposta3,'resp3')"))}}
+                {{form::label('resposta3',$resposta3)}}
+              </p>
 
-              <p>  {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta4',
-              'onclick'=>"alteraResposta($pergunta->resposta3,resposta4)"))}}
-                {{form::label('resposta4',$pergunta->resposta3)}}
+              <p id="resp4">  {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta4',
+              'onclick'=>"alteraResposta($resposta4,'resp4')"))}}
+                {{form::label('resposta4',$resposta4)}}
+              </p>
 
-              <p> {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta5',
-              'onclick'=>"alteraResposta($pergunta->resposta4,resposta5)"))}}
-                {{form::label('resposta5',$pergunta->resposta4)}}
+              <p id="resp5"> {{Form::radio('resposta','Pergunta',false,array('id'=>'resposta5',
+              'onclick'=>"alteraResposta($resposta5,'resp5')"))}}
+                {{form::label('resposta5',$resposta5)}}
+              </p>
 
                 {{Form::hidden('respostaCerta',$pergunta->resposta_certa,array('id'=>'respostaCerta'))}}
                 {{Form::hidden('respostaEscolhida','',array('id'=>'respostaEscolhida'))}}
